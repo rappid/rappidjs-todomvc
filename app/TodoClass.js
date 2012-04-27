d62c2bb9e0fa561bac6a58dbcaa5072c6a837270
Marcus Krejpowicz <krebbl@gmail.com>
2012-04-27 23:17:09 +0200

    fixed code formatting

--- app/TodoClass.js
+++ app/TodoClass.js
@@ -4,20 +4,20 @@ define(["js/core/Application", "js/core/I18n", "app/model/Todo", "app/collection
         var ENTER_KEY = 13;
 
         return Application.inherit("app.TodoClass", {
-            inject:{
-                i18n:I18n
+            inject: {
+                i18n: I18n
             },
             /**
              * Initializes the app
              * In this method we set the initial models
              */
-            initialize:function () {
+            initialize: function () {
 
                 this.set("todoList", new TodoList());
                 this.set("filterList", new ListView({
-                    list:this.get("todoList"),
-                    filter:'all',
-                    filterFnc:function (item) {
+                    list: this.get("todoList"),
+                    filter: 'all',
+                    filterFnc: function (item) {
                         var filter = this.$.filter;
                         if (filter == "active") {
                             return !item.isCompleted();
@@ -33,42 +33,42 @@ define(["js/core/Application", "js/core/I18n", "app/model/Todo", "app/collection
             /**
              * Are triggered
              */
-            showAll:function () {
+            showAll: function () {
                 this.$.filterList.set("filter", 'all');
             },
-            showActive:function () {
+            showActive: function () {
                 this.$.filterList.set("filter", "active");
             },
-            showCompleted:function () {
+            showCompleted: function () {
                 this.$.filterList.set("filter", "completed");
             },
-            isStringEqual:function (route, filter) {
+            isStringEqual: function (route, filter) {
                 return route == filter;
             },
             /**
              * The rest is just controller stuff
              */
-            addNewTodo:function (e) {
+            addNewTodo: function (e) {
                 if (e.$.keyCode === ENTER_KEY) {
                     var tmp = this.get("newTodo");
                     if (tmp.hasTitle()) {
-                        var newTodo = new Todo({title:tmp.get("title")});
+                        var newTodo = new Todo({title: tmp.get("title")});
                         newTodo.setCompleted(false);
                         this.get("todoList").add(newTodo);
                         tmp.set("title", "");
                     }
                 }
             },
-            markAllComplete:function (e, input) {
+            markAllComplete: function (e, input) {
                 this.get("todoList").markAll(input.get("checked"));
             },
-            clearCompleted:function (e) {
+            clearCompleted: function (e) {
                 this.get("todoList").clearCompleted();
             },
-            removeTodo:function (e, el) {
+            removeTodo: function (e, el) {
                 this.get("todoList").remove(e.$);
             },
-            sort:function () {
+            sort: function () {
                 this.get("todoList").sort(function (t1, t2) {
                     if (t1.get("isDone") && t2.get("isDone")) {
                         return 0;
@@ -82,11 +82,11 @@ define(["js/core/Application", "js/core/I18n", "app/model/Todo", "app/collection
             /**
              * Start the application and render it to the body ...
              */
-            start:function (parameter, callback) {
+            start: function (parameter, callback) {
                 // false - disables autostart
                 this.callBase(parameter, false);
 
-                this.$.i18n.set("locale", "en_EN", {silent:true});
+                this.$.i18n.set("locale", "en_EN", {silent: true});
                 this.$.i18n.loadLocale("en_EN", callback);
             }
         });
--- app/collection/TodoList.js
+++ app/collection/TodoList.js
@@ -1,11 +1,11 @@
 define(["js/core/List"], function (List) {
     return List.inherit("app.collection.TodoList", {
-        markAll:function (done) {
+        markAll: function (done) {
             this.each(function (todo) {
                 todo.setCompleted(done);
             });
         },
-        areAllComplete:function () {
+        areAllComplete: function () {
             if (this.$items.length === 0) {
                 return false;
             }
@@ -16,7 +16,7 @@ define(["js/core/List"], function (List) {
             }
             return true;
         }.on('change', 'add', 'remove'),
-        clearCompleted:function () {
+        clearCompleted: function () {
             console.log("clear completed");
             for (var i = this.$items.length - 1; i >= 0; i--) {
                 if (this.$items[i].isCompleted()) {
@@ -24,7 +24,7 @@ define(["js/core/List"], function (List) {
                 }
             }
         },
-        numOpenTodos:function () {
+        numOpenTodos: function () {
             var num = 0;
             for (var i = 0; i < this.$items.length; i++) {
                 if (!this.$items[i].isCompleted()) {
@@ -33,7 +33,7 @@ define(["js/core/List"], function (List) {
             }
             return num;
         }.on('change', 'add', 'remove'),
-        numCompletedTodos:function () {
+        numCompletedTodos: function () {
             var num = 0;
             for (var i = 0; i < this.$items.length; i++) {
                 if (this.$items[i].isCompleted()) {
@@ -42,7 +42,7 @@ define(["js/core/List"], function (List) {
             }
             return num;
         }.on('change', 'add', 'remove'),
-        hasCompletedTodos:function () {
+        hasCompletedTodos: function () {
             return this.numCompletedTodos() > 0;
         }.on('change', 'add', 'remove')
     });
